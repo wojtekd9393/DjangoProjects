b0566a80e71c3ab8fa62635681e59a42fa2ee898
wojtekd9393 <wojtekd9393@gmail.com>
2021-02-07 23:32:08 +0100

    CSS modification; new navbar

--- static/js/cards_handling.js
+++ static/js/cards_handling.js
@@ -10,8 +10,7 @@ $(document).ready(function(){
             data: serializedData,
             type: 'POST',
             success: function(response) {
-                $("#greenList").prepend('<div class="card text-white bg-success mb-3" id="greenCard" data-id="' + response.task.id + '"><div class="card-body">'
-                +response.task.item+ '<div style="margin-left: 270px;"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
+                $("#greenList").prepend('<div class="card text-white bg-success mb-3" id="greenCard" data-id="' + response.task.id + '"><div class="card-body"><p>'+response.task.item+'</p><div class="actions"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
                 $("#exampleModal .close").click();
             }
         })
@@ -51,8 +50,7 @@ $(document).ready(function(){
             data: serializedData,
             type: 'POST',
             success: function(response) {
-                $("#redList").prepend('<div class="card text-white bg-danger mb-3" id="redCard" data-id="' + response.task.id + '"><div class="card-body">'
-                +response.task.item+ '<div style="margin-left: 270px;"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
+                $("#redList").prepend('<div class="card text-white bg-danger mb-3" id="redCard" data-id="' + response.task.id + '"><div class="card-body"><p>'+response.task.item+'</p><div class="actions"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
                 $("#exampleModal2 .close").click();
             }
         })
@@ -92,8 +90,7 @@ $(document).ready(function(){
             data: serializedData,
             type: 'POST',
             success: function(response) {
-                $("#blueList").prepend('<div class="card text-white bg-primary mb-3" id="blueCard" data-id="' + response.task.id + '"><div class="card-body">'
-                +response.task.item+ '<div style="margin-left: 270px;"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
+                $("#blueList").prepend('<div class="card text-white bg-primary mb-3" id="blueCard" data-id="' + response.task.id + '"><div class="card-body"><p>'+response.task.item+'</p><div class="actions"><a href="/edit/'+response.task.id+'"><i class="fas fa-edit fa-white"></i></a> <i class="fas fa-trash-alt fa-white" data-id="' + response.task.id + '" data-toggle="tooltip" data-placement="bottom" title="Delete item"></i></div></div></div>')
                 $("#exampleModal3 .close").click();
             }
         })
